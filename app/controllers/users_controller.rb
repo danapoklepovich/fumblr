34384c5f2d2ae17d81ab471dbc75d5627aa2d6f1
danapoklepovich <dana.poklepovich@gmail.com>
2018-12-15 15:12:10 +0800

    changes after review

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update destroy]
-  before_action :authenticate_user!
-  protect_from_forgery prepend: true
+  before_action :set_user, only: %i[show]
 
   # GET /users
   # GET /users.json
@@ -18,10 +16,5 @@ class UsersController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def user_params
-    params.require(:user).permit(:username, :description, :avatar_url)
   end
 end
