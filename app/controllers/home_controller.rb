6bcf7e7d60d3d6c2662a8b7f02b93f49750eebd8
Elliot <elliot@Elliots-MacBook-Pro.local>
2018-12-15 14:16:38 +0800

    models finished

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      render 'dashboard'
+    else
+      render 'index'
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :follower_records, class_name: 'Follow', foreign_key: 'following_id'
+  has_many :followers, through: :follower_records
+
+  has_many :following_records, class_name: 'Follow', foreign_key: 'follower_id'
+  has_many :followings, through: :following_records
 end
