c6110a9fc7c2e08a7e9119495e9f2121448727e6
danapoklepovich <dana.poklepovich@gmail.com>
2018-12-15 15:30:41 +0800

    edit users and not null

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181215062648) do
+ActiveRecord::Schema.define(version: 20181215071808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20181215062648) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "username", null: false
     t.text "description"
     t.string "avatar_url"
     t.datetime "created_at", null: false
