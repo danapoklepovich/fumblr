15720b80d4e1abedfecd3961aeab8f87ee00e6d2
danapoklepovich <dana.poklepovich@gmail.com>
2018-12-15 14:41:25 +0800

    sign in and register users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   get 'home/index'
 
   resources :posts
-  resources :users
+  resources :users, only: [:show, :index]
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
